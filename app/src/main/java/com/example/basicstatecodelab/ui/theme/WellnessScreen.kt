5c8d71c6d6bc31decfd348c578173d5662c7047e
Javier Armenta <j.armentagarcia@fetchrewards.com>
2023-10-30 00:06:51 -0600

    UI state explanation and App Logic clasification: UI logic, Business logic

--- app/src/main/java/com/example/basicstatecodelab/ui/theme/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/ui/theme/WellnessScreen.kt
@@ -2,23 +2,46 @@ package com.example.basicstatecodelab.ui.theme
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+/*
+
+
+UI State:
+    what should display on the screen
+APP Logic:
+    how the app behaves and
+    how should react to state changes.
+
+        There are two types of logic:
+        - the UI behavior or UI logic,
+        - the business logic
+
+        1.- The UI logic:
+            - relates to how to display state changes on the screen
+                - (for example, the navigation logic or showing snackbars).
+        2.- The business logic:
+            - is what to do with state changes
+                - (for example making a payment or storing user preferences).
+
+ViewModels
+    - provide the UI state and access to the business logic located in other layers of the app.
+    - survive configuration changes, so they have a longer lifetime than the Composition.
+    - They can follow the lifecycle of the host of Compose content—that is, activities, fragments,
+        or the destination of a Navigation graph if you're using Compose Navigation.
+ */
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    wellnessViewModel: WellnessViewModel = viewModel(),
+    modifier: Modifier = Modifier
+) {
     Column {
         StatefulCounter(modifier)
-        val listState: SnapshotStateList<WellnessTask> = remember {
-            //mutableStateListOf(getWellnessTasks())
-            getWellnessTasks().toMutableStateList()
-        }
-        val onCloseTask = { task:WellnessTask -> listState.remove(task) }
+        val onCloseTask = { task:WellnessTask -> wellnessViewModel.remove(task) }
         WellnessTaskList(
-            listState = listState,
+            listState = wellnessViewModel.tasks,
             onCloseTask = onCloseTask,
         )
     }
@@ -32,4 +55,3 @@ fun WellnessScreen() {
         WellnessScreen(modifier = Modifier)
     }
 }
-private fun getWellnessTasks() = List(30){ i -> WellnessTask(i, "Task # $i")}
--- app/src/main/java/com/example/basicstatecodelab/ui/theme/WellnessTaskList.kt
+++ app/src/main/java/com/example/basicstatecodelab/ui/theme/WellnessTaskList.kt
@@ -3,14 +3,12 @@ package com.example.basicstatecodelab.ui.theme
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WellnessTaskList(
-    listState: SnapshotStateList<WellnessTask>,
-    onCloseTask: (WellnessTask) -> Boolean,
+    listState: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
     modifier: Modifier = Modifier,
 ) {
 
